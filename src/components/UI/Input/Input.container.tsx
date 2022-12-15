bf6bbf74af0e37175ddfdccd12bf4bb10b136e0c
Will-Andrade <willian.santos.andra@outlook.com>
2022-12-14 22:39:45 -0400

    fix: removes unnecessary container files

--- src/components/UI/Input/Input.container.tsx
+++
@@ -1,7 +0,0 @@
-import InputComponent, { InputComponentProps } from "./Input.component";
-
-const Input = ({ label, type, onChangeHandler }: InputComponentProps): JSX.Element => {
-  return <InputComponent label={label} type={type} onChangeHandler={onChangeHandler} />
-};
-
-export default Input;
--- src/components/User/User.container.tsx
+++
@@ -1,7 +0,0 @@
-import UserComponent, { UserProps } from "./User.component";
-
-const User = ({ username, age }: UserProps): JSX.Element => {
-  return <UserComponent username={username} age={age} />
-};
-
-export default User;
